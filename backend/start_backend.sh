80d8a10a78d1a2da43477db5342492b7aa142dd0
Harald Walker <harald@bitwalker.eu>
2026-09-09 09:35:14 +0200

    Update start_backend.sh

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -1,6 +1,6 @@
 
 # Install spatial dependencies
-apt-get -o Acquire::Check-Valid-Until=false update && apt-get install binutils libproj-dev gdal-bin libgdal-dev -y
+apt-get -o Acquire::Check-Valid-Until=false update && apt-get install binutils libproj-dev gdal-bin libgdal-dev -y --fix-missing
 ldconfig
 
 # Install pdm
